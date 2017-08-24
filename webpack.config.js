f54570c31d66e39e4d659d43681254b8c87a455e
Mashiro Wang <hyperoot@gmail.com>
2017-08-24 13:45:06 +0800

    Typescript-Webpack-Boilerplate First Commit

---
+++ webpack.config.js
@@ -0,0 +1,72 @@
+const path = require('path')
+const webpack = require('webpack')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+const nodeModulesPath = path.resolve(__dirname, 'node_modules')
+const isProduction = process.env.NODE_ENV == 'production'
+
+const config = {
+  entry: {
+    vendors: [
+      'react',
+      'react-dom',
+      'babel-polyfill'
+    ],
+    app: [
+      path.resolve(__dirname, 'src', 'Index.tsx')
+    ]
+  },
+
+  resolve: {
+    extensions: ['.tsx', '.ts', '.js', '.css'],
+    modules: ['node_modules']
+  },
+  
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].[hash].js'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        enforce: 'pre', // 分类，preloader
+        loader: 'tslint-loader',
+        options: { emitErrors: true }
+      },
+      {
+        test: /\.tsx?$/,
+        loaders: ['babel-loader?cacheDirectory', 'awesome-typescript-loader?tsconfig.tsconfig.webpack.json&useCache=true']
+      },
+      {
+        test: /\.css$/,
+        loaders: ['style-loader', 'css-loader?minimize']
+      },
+      {
+        test: /\.(jpg|png|woff|eot|ttf|svg|gif)$/,
+        loader: "file-loader?name=[name].[hash].[ext]"
+      }
+    ]
+  },
+
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({ name: 'vendors', filename: '[name].[hash].js' }),
+    new HtmlWebpackPlugin({ template: 'index.html' }),
+    new webpack.DefinePlugin({ DEBUG: true })
+  ]
+}
+
+if (isProduction) {
+  config.plugins.push(new webpack.optimize.UglifyJsPlugin({
+    compress: {
+      warnings: false
+    }
+  }))
+  config.plugins.push(new webpack.DefinePlugin({
+    'process.env': { NODE_ENV: '"prodection"' },
+    DEBUG: false
+  }))
+}
+
+module.exports = config
---
+++ webpack.dev.config.js
@@ -0,0 +1,51 @@
+const webpack = require('webpack')
+const { Config } = require('webpack-config')
+const path = require('path')
+
+const mainConfig = new Config().extend('webpack.config')
+mainConfig.module.rules = []
+
+const devConfigExtension = {
+  entry: {
+    app: [
+      'webpack-dev-server/client?http://localhost:8848',
+      'webpack/hot/only-dev-server'
+    ]
+  },
+  output: {
+    filename: '[name].js',
+    publicPath: 'http://localhost:8848'
+  },
+  devtool: 'eval-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        enforce: 'pre',
+        loader: 'tslint-loader',
+        options: { emitErrors: true },
+        include: path.resolve(__dirname, 'src')
+      },
+      {
+        test: /\.tsx?$/,
+        loaders: ['react-hot-loader', 'babel-loader?cacheDirectory', 'awesome-typescript-loader?tsconfig=tsconfig.webpack.json&useCache=true']
+      },
+      {
+        test: /\.css$/,
+        loaders: ['style-loader', 'css-loader']
+      },
+      {
+        test: /\.(jpg|png|woff|eot|ttf|svg|gif)$/,
+        loader: "file-loader?name=[name].[ext]"
+      }
+    ]
+  },
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.DefinePlugin({
+      DEBUG: true
+    })
+  ]
+}
+
+module.exports = mainConfig.merge(devConfigExtension)
